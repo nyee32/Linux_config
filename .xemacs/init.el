312c5a3d62ace60e67f04d96bb3652657e0f76f1
Nathan <nyee32@gmail.com>
2016-12-20 22:29:45 -0800

    emacs will now install missing elpa packages

--- .xemacs/init.el
+++ .xemacs/init.el
@@ -230,10 +230,25 @@
 (put 'upcase-region 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
 
+; list the packages you want
+(setq package-list '(yasnippet auto-complete))
+
 ;; MELPA Packages
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; Start yasnippet at start
 (add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.11.0")
