8584f7bef878a10c7e1eb1b47f4b17ca40baa45c
Nathan <nyee32@gmail.com>
2016-12-20 16:24:02 -0800

    update to yasnippit version

--- .xemacs/init.el
+++ .xemacs/init.el
@@ -235,7 +235,7 @@
 	(setq yas-dont-activate t)))
 
 ;; Start yasnippet at start
-(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.10.0")
+(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.11.0")
 (require 'yasnippet)
 (yas/initialize)
 
