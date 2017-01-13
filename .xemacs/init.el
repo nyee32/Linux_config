a4b3a79697491e2f552cef006afad29d08faaf32
nyee32 <nyee32@gmail.com>
2017-01-12 23:01:54 -0800

    added window numbering to init

--- .xemacs/init.el
+++ .xemacs/init.el
@@ -353,3 +353,8 @@
 (add-hook 'term-mode-hook
           (lambda ()
             (setq show-trailing-whitespace nil)))
+
+(require 'window-numbering)
+;; highlight the window number in pink color
+(custom-set-faces '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold)))))
+(window-numbering-mode 1)
